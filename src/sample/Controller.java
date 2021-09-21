dcfdfe09254b1a11587a5102e08a7a33dda357eb
Lucas Gomes <lggomes007@gmail.com>
2021-09-21 11:10:40 -0300

    Putting image files and some buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,55 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Controller {
+
+    @FXML
+    private ImageView dayView;
+    @FXML
+    private ImageView circle;
+    @FXML
+    private ImageView notification;
+    @FXML
+    private ImageView warning;
+    @FXML
+    private ImageView wifi;
+    @FXML
+    private ImageView bt;
+    @FXML
+    private ImageView thermometerIcon;
+    @FXML
+    private ImageView clock;
+    @FXML
+    private ImageView totalcross;
+    @FXML
+    private ImageView torizon;
+    @FXML
+    private ImageView dayViwe;
+    @FXML
+    private Button powerOf;
+    @FXML
+    private Button calendar;
+    @FXML
+    private Button tools;
+    @FXML
+    private Button thermometer;
+    @FXML
+    private Button home;
+    @FXML
+    private Button plus;
+    @FXML
+    private Button minus;
+    @FXML
+    private Button day;
+    @FXML
+    private Button night;
+
 }
