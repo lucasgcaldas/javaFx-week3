834891d4e518a06b31438ba8bb5a22490e827404
Lucas Gomes <lggomes007@gmail.com>
2021-09-22 22:57:15 -0300

    Adjusting the image of the buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,9 +6,11 @@ import javafx.animation.Timeline;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Popup;
 import javafx.util.Duration;
 
 import java.time.LocalTime;
@@ -17,21 +19,49 @@ import java.time.format.DateTimeFormatter;
 public class Controller {
 
     @FXML
+    private Button day;
+    @FXML
+    private Button night;
+
+    @FXML
     private ImageView background;
 
     Image dayImage = new Image(getClass().getResourceAsStream("assets/background_day.png"));
-
+    ImageView dayButtonOn = new ImageView(getClass().getResource("assets/day_on_button.png").toExternalForm());
+    ImageView dayButtonOff = new ImageView(getClass().getResource("assets/day_off_button.png").toExternalForm());
     Image nightImage = new Image(getClass().getResourceAsStream("assets/background_night.png"));
+    ImageView nightButtonOn = new ImageView(getClass().getResource("assets/night_on_button.png").toExternalForm());
+    ImageView nightButtonOff = new ImageView(getClass().getResource("assets/night_off_button.png").toExternalForm());
+
     public void changeBackgroundDay() {
+        day.setGraphic(dayButtonOn);
+        night.setGraphic(nightButtonOff);
+        setWidthAndHeight();
+
         background.setImage(dayImage);
         background.setFitHeight(664);
         background.setFitWidth(882);
     }
 
     public void changeBackgroundNight() {
+        night.setGraphic(nightButtonOn);
+        day.setGraphic(dayButtonOff);
+        setWidthAndHeight();
+
         background.setImage(nightImage);
         background.setFitHeight(654);
         background.setFitWidth(972);
+    }
+
+    private void setWidthAndHeight() {
+        dayButtonOn.setFitWidth(35);
+        dayButtonOn.setFitHeight(30);
+        dayButtonOff.setFitWidth(35);
+        dayButtonOff.setFitHeight(30);
+        nightButtonOn.setFitWidth(35);
+        nightButtonOn.setFitHeight(30);
+        nightButtonOff.setFitWidth(35);
+        nightButtonOff.setFitHeight(30);
     }
 
     public void turnOff() {
@@ -45,9 +75,12 @@ public class Controller {
     private Label temperature;
 
     IntegerProperty property = new SimpleIntegerProperty();
-
     @FXML
     public void initialize() {
+        day.setGraphic(dayButtonOn);
+        dayButtonOn.setFitWidth(35);
+        dayButtonOn.setFitHeight(30);
+
         property.set(16);
         temperature.textProperty().bind(property.asString());
 
@@ -57,7 +90,7 @@ public class Controller {
             LocalTime currentTime = LocalTime.now();
             time.setText(dtf.format(currentTime));
         }),
-                new KeyFrame(Duration.seconds(1))
+            new KeyFrame(Duration.seconds(1))
         );
         clock.setCycleCount(Animation.INDEFINITE);
         clock.play();
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,16 +6,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         primaryStage.setTitle("Home Appliance");
         primaryStage.setScene(new Scene(root, 800, 480));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
